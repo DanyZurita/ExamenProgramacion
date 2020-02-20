f5d3b04699391130a34329ed08a0c6cf0056933d
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-02-20 18:09:50 +0000

    feat(UfosPark): Aadir dispatch() asginando un ovni

--- src/main/java/edu/elsmancs/ExamenProgramacion/UfosPark.java
+++ src/main/java/edu/elsmancs/ExamenProgramacion/UfosPark.java
@@ -1,18 +1,26 @@
 package edu.elsmancs.ExamenProgramacion;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UfosPark {
     private double fee;
-    public ArrayList<String> flota;
+    public Map<String, String> flota = new HashMap<String, String>();
     
     public UfosPark() {}
     
     public void add(String ufo) {
-        this.flota.add(ufo);
+        this.flota.putIfAbsent(ufo, "libre");
     }
     
     void dispatch(CreditCard cc) {
         cc.pay(fee);
+        for (String ovni : flota.keySet()) {
+            if (flota.get(ovni).equals("libre")) {
+                flota.put(ovni, cc.number());
+                break;
+            }
+        }
+        
     }
 }
