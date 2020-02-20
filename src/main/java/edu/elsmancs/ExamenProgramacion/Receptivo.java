97395c4cedae94165f5b8c6aab3367bf64a715a0
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-02-20 20:03:10 +0000

    feat(Receptivo): Aadir registrar()

--- src/main/java/edu/elsmancs/ExamenProgramacion/Receptivo.java
+++ src/main/java/edu/elsmancs/ExamenProgramacion/Receptivo.java
@@ -1,9 +1,15 @@
 package edu.elsmancs.ExamenProgramacion;
 
+import java.util.ArrayList;
+
 
 public class Receptivo {
     
+    private ArrayList<GuestDispatcher> servicios = new ArrayList<>();
+    
     public Receptivo() {}
     
-    
+    void registra(GuestDispatcher dispatch) {
+        servicios.add(dispatch);
+    }
 }
