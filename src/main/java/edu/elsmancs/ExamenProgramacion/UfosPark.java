752c21b88d5849841d91fc41093ff54cd82ea01a
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-02-20 19:03:12 +0000

    refactor(UfosPark): Refactorizar dispatch() para que no cobre doble

--- src/main/java/edu/elsmancs/ExamenProgramacion/UfosPark.java
+++ src/main/java/edu/elsmancs/ExamenProgramacion/UfosPark.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UfosPark {
-    private double fee;
+    private double fee = 500.0;
     public Map<String, String> flota = new HashMap<String, String>();
     
     public UfosPark() {}
@@ -13,19 +13,28 @@ public class UfosPark {
         this.flota.putIfAbsent(ufo, ufo);
     }
     
+    boolean assigned = false;
     void dispatch(CreditCard cc) {
-        cc.pay(fee);
-        for (String ufo : flota.keySet()) {
-            if (flota.get(ufo).equals(ufo)) {
-                flota.put(cc.number(), ufo);
-                break;
+        if (!assigned) {
+            cc.pay(fee);
+            for (String ufo : flota.keySet()) {
+                if (this.flota.get(ufo).equals(ufo)) {
+                    this.flota.put(cc.number(), ufo);
+                    assigned = true;
+                    break;
+                }
             }
         }
-        
     }
     
-    String getUfoOf(String owner) {
-        return flota.get(owner);
+    String getUfoOf(String owner){
+        try {
+            return this.flota.get(owner);
+        }
+        catch (Exception e) {
+            return "No existe";
+        }
+        
             
     } 
     
