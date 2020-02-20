77d4a10f3395c93ab686f3db7637dc9a6f01e896
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-02-20 19:31:12 +0000

    feat(CrystalExpender): Aadir clase CrystalExpender, variabels y contructor

---
+++ src/main/java/edu/elsmancs/ExamenProgramacion/CrystalExpender.java
@@ -0,0 +1,26 @@
+package edu.elsmancs.ExamenProgramacion;
+
+public class CrystalExpender {
+    private int stock = 0;
+    private double itemCost = 0.0;
+    
+    public CrystalExpender(int stock, double itemCost) {
+        this.stock = stock;
+        this.itemCost = itemCost;
+    }
+    
+    void dispatch(CreditCard cc) {
+        if (0 <= stock) {
+            if (cc.pay(itemCost)) {
+                this.stock -= 1;
+            }
+        }
+        
+    }
+    
+    @Override
+    public String toString() {
+        return "Stock: " + this.stock + '\n'
+                + "Cost: " + this.itemCost + '\n';
+    }
+}
