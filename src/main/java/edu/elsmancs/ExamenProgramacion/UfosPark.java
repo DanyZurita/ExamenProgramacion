958d8bebd6b511ddc4fed6cbf9097a1453d45998
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-02-27 11:00:33 +0000

    fix(UfosPark): Arreglar el dispatch() y getUfoOf()

--- src/main/java/edu/elsmancs/ExamenProgramacion/UfosPark.java
+++ src/main/java/edu/elsmancs/ExamenProgramacion/UfosPark.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class UfosPark implements GuestDispatcher {
     private final double fee = 500.0;
-    public Map<String, String> flota = new HashMap<String, String>();
+    public Map<String, CreditCard> flota = new HashMap<String, CreditCard>();
     
     public UfosPark() {}
     
@@ -17,18 +17,21 @@ public class UfosPark implements GuestDispatcher {
     
     @Override
     public void dispatch(CreditCard credit) {   
-    for (String ufo : flota.keySet()) {
-        if (this.flota.get(ufo) == null) {
-            credit.pay(fee);
-            this.flota.compute(ufo, (k,v) -> v = credit.number());
-            break;
+        for (String ufo : flota.keySet()) {
+            if (this.flota.get(ufo) == null) {
+                if (!flota.containsValue(credit)){
+                    if (credit.pay(fee)){
+                        flota.put(ufo, credit);
+                        break;
+                    }
+                }
             }
         }
     }
     
     String getUfoOf(String owner){
         for (String ufo : flota.keySet()) {
-            if (flota.get(ufo).equals(owner)){
+            if (flota.get(ufo) != null && flota.get(ufo).number().equals(owner)){
                 return ufo;
             }
         }
