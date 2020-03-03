a0a710c0fd8e6141a8862128a079c48a5bd3a77f
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-03-03 08:56:06 +0000

    feat(RickMenuDispatcher): Addir atributos e imports de List y ArrayList

--- src/main/java/edu/elsmancs/ExamenProgramacion/RickMenuDispatcher.java
+++ src/main/java/edu/elsmancs/ExamenProgramacion/RickMenuDispatcher.java
@@ -1,6 +1,13 @@
 package edu.elsmancs.ExamenProgramacion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class RickMenuDispatcher {
-
+   private int stock = 100;
+   private double menuCost = 10.0;
+   private final List<String> orders = new ArrayList<>();
+   
+   
 }
