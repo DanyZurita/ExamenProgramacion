754bdaece25d1c1b02a8b117d56d8479f5301844
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-03-03 08:58:57 +0000

    feat(RickMenuDispatcher): Aadir implements de GuestDispatcher y dispatch()

--- src/main/java/edu/elsmancs/ExamenProgramacion/RickMenuDispatcher.java
+++ src/main/java/edu/elsmancs/ExamenProgramacion/RickMenuDispatcher.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class RickMenuDispatcher {
+public class RickMenuDispatcher implements GuestDispatcher{
    private int stock = 100;
    private double menuCost = 10.0;
    private final List<String> orders = new ArrayList<>();
    
    RickMenuDispatcher() {};
+   
+   @Override
+    public void dispatch(CreditCard credit) {
+        if (stock > 0 && credit.pay(menuCost)) {
+            stock -= 1;
+            orders.add(credit.owner());
+        }
+    }
+    
 }
