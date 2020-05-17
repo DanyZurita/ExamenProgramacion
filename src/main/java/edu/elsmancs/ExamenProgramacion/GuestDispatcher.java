14dce2c9590c8f953850639dcdd56e561d06460d
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-17 15:49:52 +0200

    docs(GuestDispatcher): Añadir comentarios javadoc

--- src/main/java/edu/elsmancs/ExamenProgramacion/GuestDispatcher.java
+++ src/main/java/edu/elsmancs/ExamenProgramacion/GuestDispatcher.java
@@ -1,8 +1,26 @@
+/**
+ * GuestDispatcher
+ * ==============
+ * Interface para generalizar el método dispatch()
+ */
 package edu.elsmancs.ExamenProgramacion;
 
+/**
+ *
+ * @author DanyZurita
+ * 
+ */
 
  interface GuestDispatcher {
     
+    /**
+     * Despachar
+     * 
+     * Despachar un servicio cobrandolo a la CreditCard.
+     * 
+     * @param CreditCard Targeta donde se cobran los servicios.
+     * @return void
+     */
     void dispatch(CreditCard credit);
     
 }
